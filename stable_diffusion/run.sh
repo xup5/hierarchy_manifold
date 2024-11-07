06df322908663b81b160f9a496f9b09b729b01c4
xup5 <panxu001@gmail.com>
2024-11-07 01:27:51 -0500

    fix run.sh

--- stable_diffusion/run.sh
+++ stable_diffusion/run.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-export HUGGING_FACE_HUB_TOKEN=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
-
 module load python
 
 mamba activate /n/holylabs/LABS/sompolinsky_lab/Everyone/xupan/envs/diffusers
